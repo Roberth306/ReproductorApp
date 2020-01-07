dca46466cfd05b16431ab63959903c13981be31c
Roberth <roberth.e.rrc@gmail.com>
2020-01-07 08:14:26 +0100

    asdf

--- app/src/main/java/tk/roberthramirez/rpmusica/MainActivity.java
+++ app/src/main/java/tk/roberthramirez/rpmusica/MainActivity.java
@@ -1,5 +1,6 @@
 package tk.roberthramirez.rpmusica;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,24 +10,38 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity implements IMusicaListener, View.OnClickListener{
     private RecyclerView rvMusica;
     private MusicObject[] musicObjects;
+    private int[] idMusic;
     private PlayerService mService;
 
+    private SeekBar seekBar;
     private ImageButton atras;
     private ImageButton stopPlay;
     private ImageButton siguiente;
+    private boolean isPaused = true;
+
+    private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    // Runnable update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +58,51 @@ public class MainActivity extends AppCompatActivity implements IMusicaListener, 
         atras = findViewById(R.id.bAnterior);
         stopPlay = findViewById(R.id.bPlayPause);
         siguiente = findViewById(R.id.bSiguiente);
+        seekBar = findViewById(R.id.sbProgress);
 
         atras.setOnClickListener(this);
         stopPlay.setOnClickListener(this);
         siguiente.setOnClickListener(this);
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mService.cambioTiempo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
         Intent intent = new Intent(this, PlayerService.class);
+        //todo iniciar servicio para que no acabe
         bindService(intent, connection, Context.BIND_AUTO_CREATE);
+
+        executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "a"+seekBar.getMax(), Toast.LENGTH_SHORT).show();
+                Log.i("aaaaaaaaaa", "bbbbbbbbbbbbbb");
+            }
+        }, 0, 1000, TimeUnit.MILLISECONDS);
+
+
+        //Toast.makeText(this, executor.isShutdown()+" "+executor.isTerminated(), Toast.LENGTH_LONG).show();
     }
 
-    public void inicializarDatos() {
+    /*public void inicializarDatos() {
         File[] files = null;
         Uri ubiRaw = Uri.parse("android.resource://"+getPackageName()+"/raw");
 
@@ -69,15 +114,28 @@ public class MainActivity extends AppCompatActivity implements IMusicaListener, 
             musicObjects[i] = new MusicObject(files[i].getName(), Uri.parse(files[i].getPath()));
         }
 
-    }
-    public void inicializarRawResources() {
+    }*/
+    public void inicializarRawResources(){
          String r1 = getResources().getResourceEntryName(R.raw.infected_mushroom);
-         String r2 = getResources().getResourceName(R.raw.maid_dragon);
 
          musicObjects = new MusicObject[2];
          musicObjects[0] = new MusicObject(getResources().getResourceEntryName(R.raw.infected_mushroom), Uri.parse(getResources().getResourceName(R.raw.infected_mushroom)));
-         musicObjects[1] = new MusicObject(getResources().getResourceEntryName(R.raw.maid_dragon), Uri.parse(getResources().getResourceName(R.raw.maid_dragon)));
+
+        Field[] fields = R.raw.class.getFields();
+        idMusic = new int[fields.length];
+
+        for(int i=0; i<fields.length;i++) {
+            int resourceID=0;
+            try{
+               resourceID =fields[i].getInt(fields[i]);
+            }catch (IllegalAccessException ie){
+
+            }
+            idMusic[i] = resourceID;
+        }
+
     }
+
 
     private ServiceConnection connection = new ServiceConnection() {
 
@@ -91,7 +149,7 @@ public class MainActivity extends AppCompatActivity implements IMusicaListener, 
             if(mService.isActive()){
                 //todo actualizar actividad
             }else {
-                mService.inicializarLista(musicObjects);
+                mService.inicializarLista(idMusic);
             }
 
         }
@@ -106,19 +164,44 @@ public class MainActivity extends AppCompatActivity implements IMusicaListener, 
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.bAnterior:
-
+                mService.anteriorM();
+                inicializarSeekBar();
                 break;
             case R.id.bPlayPause:
-
+                if(isPaused){
+                    stopPlay.setImageResource(R.drawable.ic_pause_black_18dp);
+                    mService.resume();
+                    isPaused=false;
+                }else{
+                    stopPlay.setImageResource(R.drawable.ic_play_arrow_black_18dp);
+                    mService.pause();
+                    isPaused=true;
+                }
                 break;
             case R.id.bSiguiente:
-
+                mService.siguienteM();
+                inicializarSeekBar();
                 break;
         }
     }
 
     @Override
     public void onMusicaSeleccionada(int position) {
-        //todo llamar servicio
+        Toast.makeText(this, "S"+position, Toast.LENGTH_LONG).show();
+        mService.musicaSeleccionada(position);
+        inicializarSeekBar();
+    }
+
+
+    public void inicializarSeekBar() {
+        //todo llamar metodo para obtener tiempo total cancion
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            seekBar.setMin(0);
+        }
+        seekBar.setProgress(0);
+        seekBar.setMax(mService.consultaTotalTiempo());
+        Toast.makeText(this, "a"+seekBar.getMax(), Toast.LENGTH_LONG).show();
+
+
     }
 }
--- app/src/main/java/tk/roberthramirez/rpmusica/PlayerService.java
+++ app/src/main/java/tk/roberthramirez/rpmusica/PlayerService.java
@@ -11,16 +11,27 @@ import androidx.annotation.Nullable;
 
 import java.io.IOException;
 
-public class PlayerService extends Service implements MediaPlayer.OnPreparedListener{
+public class PlayerService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener{
     private Thread playerT = null;
     private final IBinder binder = new LocalBinder();
-    private MusicObject[] musicObjects;
+
+    private int[] idMusica;
+    //private MusicObject[] musicObjects;
+
     private boolean isOld = false;
     private MediaPlayer mediaPlayer = null;
 
     private int puntero = 0;
 
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
 
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        stopSelf();
+    }
 
 
     public class LocalBinder extends Binder {
@@ -32,61 +43,98 @@ public class PlayerService extends Service implements MediaPlayer.OnPreparedList
     @Override
     public void onCreate() {
         super.onCreate();
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try{
-            mediaPlayer.setDataSource(getApplicationContext(), musicObjects[puntero].getUbicacion());
-        }catch (IOException ioe) {
-            System.out.println("Errror");
-        }
-        mediaPlayer.setOnPreparedListener(this);
+
+
     }
 
     @Override
     public int onStartCommand(final Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-        if(playerT == null || !playerT.isAlive()){
+        /*if(playerT == null || !playerT.isAlive()){
             playerT = new Thread(new Runnable() {
                 @Override
                 public void run() {
 
                 }
             });
-        }
+        }*/
+
+        //mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        //mediaPlayer.start();
+        //mediaPlayer.setOnPreparedListener(this);
+        //mediaPlayer.prepareAsync();
+
 
         return START_STICKY;
     }
 
     @Override
     public void onPrepared(MediaPlayer mp) {
-
+        mp.start();
     }
 
-    public void inicializarLista(MusicObject[] musicObjects) {
-        this.musicObjects = musicObjects;
-    }
+    public void inicializarLista(int[] idMusica) {
+        //this.musicObjects = musicObjects;
+        /*try{
+            mediaPlayer.setDataSource();
+        }catch (IOException ioe) {
+            System.out.println("Errror");
+        }*/
 
+        this.idMusica = idMusica;
+        mediaPlayer = MediaPlayer.create(this, idMusica[puntero]);
+
+    }
     public void siguienteM() {
         puntero++;
-        if(puntero>musicObjects.length-1){
+        if(puntero>idMusica.length-1){
             puntero=0;
         }
         //todo poner play musica
+        mediaPlayer.reset();
+        play(idMusica[puntero]);
+
     }
 
     public void anteriorM() {
         puntero--;
         if(puntero<0){
-            puntero=musicObjects.length-1;
+            puntero=idMusica.length-1;
         }
         //todo poner play musica
+        mediaPlayer.reset();
+        play(idMusica[puntero]);
     }
 
-    public void play() {
+    public void play(final int idMusic) {
+        mediaPlayer =  MediaPlayer.create(this,idMusic);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                puntero++;
+                if(puntero>=idMusica.length){
+                    puntero=0;
+                    mediaPlayer.stop();
+                }else{
+                    mediaPlayer.reset();
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(),idMusica[puntero]);
+                }
+            }
+        });
 
+        mediaPlayer.start();
     }
 
-    public void stop() {
+    public void pause() {
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+
+    }
+    public void resume() {
+        if(!mediaPlayer.isPlaying()){
+            mediaPlayer.start();
+        }
 
     }
 
@@ -97,6 +145,29 @@ public class PlayerService extends Service implements MediaPlayer.OnPreparedList
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        if(mediaPlayer!=null){
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer=null;
+        }
+    }
+    public void musicaSeleccionada(int position){
+        puntero = position;
+        mediaPlayer.reset();
+        play(idMusica[puntero]);
+    }
+
+    public int consultaTiempo() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    public int consultaTotalTiempo() {
+        return mediaPlayer.getDuration();
+    }
+
+    public void cambioTiempo(int tiempo) {
+        mediaPlayer.seekTo(tiempo);
     }
 
     @Nullable
@@ -104,4 +175,6 @@ public class PlayerService extends Service implements MediaPlayer.OnPreparedList
     public IBinder onBind(Intent intent) {
         return binder;
     }
+
+
 }
